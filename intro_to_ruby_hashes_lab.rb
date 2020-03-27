5ce56ad96ede1861280bb6457c3de0b73c139df1
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-27 19:21:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,7 +61,9 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {},
+	      :reading_railroad => {
+	        "mortgage value" => 
+	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
