927e9b2ec4f9392e984d42e39d1d2a6d172fa11a
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-27 19:09:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -77,7 +77,9 @@ def monopoly_with_fourth_tier
 	     :b_and_o_railroad => {
 	       "mortgage_value" => "$400"
 	     },
-	     :shortline => {}
+	     :shortline => {
+	       "mortgage_value" => "$800"
+	     }
 	   },
 	}
 
